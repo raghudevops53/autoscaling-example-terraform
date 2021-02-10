d6f398805cc76ee33892023e22494e393143e1a7
KalluruS <>
2021-02-10 08:13:51 +0530

    Update autoscaling-example-terraform/

--- asg.tf
+++ asg.tf
@@ -29,7 +29,7 @@ provider "aws" {
 resource "aws_autoscaling_policy" "bat" {
   name                   = "cpu-based"
   scaling_adjustment     = 4
-  adjustment_type        = "ChangeInCapacity"
+  adjustment_type        = "TargetTrackingScaling"
   cooldown               = 300
   autoscaling_group_name = aws_autoscaling_group.asg.name
   target_tracking_configuration {
